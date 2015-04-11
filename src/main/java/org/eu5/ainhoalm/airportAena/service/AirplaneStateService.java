284b1badfe9359a3a8517fc71cf2a22b83a303e9
AinhoaLM <pimplitaldea@yahoo.es>
2015-04-11 20:24:37 +0200

    IOC en clase Service de AirplaneSAte
    quitadas las clases DAO factory, con ioc y spring ya no hacen falta(las
    habíamos construido para poder elegir implementación hibernate,
    JPA,JDBC....)

--- src/main/java/org/eu5/ainhoalm/airportAena/service/AirplaneStateService.java
+++ src/main/java/org/eu5/ainhoalm/airportAena/service/AirplaneStateService.java
@@ -3,6 +3,8 @@ package org.eu5.ainhoalm.airportAena.service;
 import java.util.List;
 
 
+
+import org.eu5.ainhoalm.airportAena.dao.AirplaneStateDAO;
 import org.eu5.ainhoalm.airportAena.model.AirplaneState;
 
 public interface AirplaneStateService {
@@ -13,7 +15,8 @@ public interface AirplaneStateService {
 	public AirplaneState findByKeyAirplaneState(int code);
 	public AirplaneState findByIdAirplaneState(Long id);
 	
-	
+	public AirplaneStateDAO getAirplaneStateDAO();
+	public void setAirplaneSateDAO(AirplaneStateDAO airplaneStateDAO);
 }
 
 
--- src/test/java/org/eu5/ainhoalm/airportAena/service/impl/AirplaneStateServiceTest.java
+++ src/test/java/org/eu5/ainhoalm/airportAena/service/impl/AirplaneStateServiceTest.java
@@ -5,30 +5,36 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
+import org.eu5.ainhoalm.airportAena.dao.AirplaneStateDAO;
 import org.eu5.ainhoalm.airportAena.model.AirplaneState;
 import org.eu5.ainhoalm.airportAena.service.AirplaneStateService;
 import org.eu5.ainhoalm.airportAena.utils.HibernateUtil;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AirplaneStateServiceTest {
-	static AirplaneStateService airplaneStateSERVICE=null;
-	
+	static AirplaneStateService service=null;
+	static AirplaneStateDAO dao= null;
+	static private ClassPathXmlApplicationContext factoria =new ClassPathXmlApplicationContext("aplicationContext.xml");
 
 	@BeforeClass  
 	public static void setUpClass() throws Exception {   
-		airplaneStateSERVICE= new AirplaneStateServiceImpl();
+		service=(AirplaneStateService)factoria.getBean("airplaneStateSERVICE");
+		dao=(AirplaneStateDAO)factoria.getBean("airplaneStateDAO");
+		service.setAirplaneSateDAO(dao);
 	}  
 	
 	@AfterClass  
 	public static void tearDownClass() throws Exception {   
 		HibernateUtil.getSessionFactory().close();  
+		factoria.close();
 	} 
 	
 	@Test
 	public void findAllAirplaneState() throws Exception {
-		List<AirplaneState> listOfObj = airplaneStateSERVICE.findAllAirplaneState();
+		List<AirplaneState> listOfObj = service.findAllAirplaneState();
 		ImprimirListado(listOfObj);  
 		assertNotNull("Account list is null.", listOfObj);    
 	}
